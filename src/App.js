a7802a690d600a115b7333f5ef404b1d15a395a3
torsumkhan <torsum.khan@gmail.com>
2022-06-25 02:10:43 +0500

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import pages
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
+
+//import App styles
 import "./App.css";
 
 function App() {
-  return <div className="App">Food Recipe</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
